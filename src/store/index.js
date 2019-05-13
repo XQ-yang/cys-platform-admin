db4ad74f8458b92e86e2a3cb7367004de424a55c
thomas <cy_laq_2011@126.com>
2019-05-13 15:21:27 +0800

    1.删除业务状态 2.删除业务路由

--- src/store/index.js
+++ src/store/index.js
@@ -4,16 +4,7 @@ import Vuex from 'vuex'
 import user from './module/user'
 import app from './module/app'
 import router from './module/router'
-import sample from './module/sample'
-import storage from './module/storage'
-import outstorage from './module/outstorage'
-import outApply from './module/outApply'
-import outStock from './module/outStock'
-import outReview from './module/outReview'
-import outSignature from './module/outSignature'
-import retrospect from './module/retrospect'
 import getters from './getters'
-import flowStop from './module/flowStop'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
@@ -29,16 +20,7 @@ export default new Vuex.Store({
   modules: {
     user,
     app,
-    router,
-    sample,
-    storage,
-    outstorage,
-    outApply,
-    outStock,
-    outReview,
-    outSignature,
-    retrospect,
-    flowStop
+    router
   },
   getters
 })
