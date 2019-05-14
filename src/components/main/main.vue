2aea17492abe69f45dab104c4dd201000e432b60
thomas <cy_laq_2011@126.com>
2019-05-14 14:40:39 +0800

    1.绝对定位导航头部

--- src/components/main/main.vue
+++ src/components/main/main.vue
@@ -1,9 +1,9 @@
 <template>
   <Layout style="height: 100%" class="main">
-    <Sider hide-trigger collapsible :width="256" :collapsed-width="64" v-model="collapsed" class="left-sider" :style="{overflow: 'hidden'}">
+    <Sider hide-trigger collapsible :width="256" :collapsed-width="64" v-model="collapsed" class="left-sider left-margin-top" :style="{overflow: 'hidden'}">
       <side-menu accordion ref="sideMenu" :active-name="$route.name" :collapsed="collapsed" @on-select="turnToPage" :menu-list="menuList">
         <!-- 需要放在菜单上面的内容，如Logo，写在side-menu标签内部，如下 -->
-        <div class="logo-con">
+        <div class="logo-con header-fixed">
           <img v-show="!collapsed" :src="maxLogo" key="max-logo" />
           <img v-show="collapsed" :src="minLogo" key="min-logo" />
         </div>
@@ -203,5 +203,19 @@ export default {
 <style scoped>
     .layout-footer-center{
         text-align: center;
+    }
+    .header-fixed{
+      position: absolute;
+      top:0;
+      left:1px;
+      background: #2b3042;
+      z-index:999;
+      transition: width 0.28s;
+    }
+    .left-margin-top .ivu-menu-vertical{
+      margin-top:64px;
+    }
+    .side-menu-wrapper {
+      margin-top:64px;
     }
 </style>
--- src/router/routers.js
+++ src/router/routers.js
@@ -226,6 +226,30 @@ export const routes = [
         component: () => import('@/view/system/module')
       }
     ]
+  },
+  {
+    path: '/401',
+    name: 'error_401',
+    meta: {
+      hideInMenu: true
+    },
+    component: () => import('@/view/error-page/401.vue')
+  },
+  {
+    path: '/500',
+    name: 'error_500',
+    meta: {
+      hideInMenu: true
+    },
+    component: () => import('@/view/error-page/500.vue')
+  },
+  {
+    path: '*',
+    name: 'error_404',
+    meta: {
+      hideInMenu: true
+    },
+    component: () => import('@/view/error-page/404.vue')
   }
 ]
 // 错误处理页面
