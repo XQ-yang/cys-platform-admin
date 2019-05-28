dbefebb7a7eddc82930b98717efd8ec2d70660d9
thomas <cy_laq_2011@126.com>
2019-05-28 14:09:05 +0800

    1.优化树形选择上级菜单 2.添加前端验证

--- src/view/system/menu/meun-add-or-update.vue
+++ src/view/system/menu/meun-add-or-update.vue
@@ -8,38 +8,39 @@
       :mask-closable="false"
       width="960"
       >
-        <Form ref="menuModal" :label-width="100">
+        <Form ref="menuModal" :model="dataForm" :rules="rules" :label-width="100">
           <Form-item label="类型" prop="type" >
-            <RadioGroup>
+            <RadioGroup v-model="dataForm.type">
               <Radio :label="0">菜单</Radio>
               <Radio :label="1">按钮</Radio>
             </RadioGroup>
           </Form-item>
           <Form-item label="名称" prop="title">
-            <Input type="text"  :maxlength="8"></Input>
+            <Input v-model="dataForm.title" type="text"  :maxlength="8"></Input>
           </Form-item>
-          <Form-item label="上级菜单" >
-            <tree-select
-            v-model="treeSelected"
-            :data="tableData"
-            @on-change="handleTreeSelectChange"
-            @on-toggle-expand="handleTreeSelectExpand"
-            @on-check-change="handleTreeSelectCheckChange"
-            @on-select-change="handleTreeSelectClick"
-            >
-            </tree-select>
+          <Form-item label="上级菜单" prop="parentName">
+            <Poptip trigger='click' v-model="popVisible" placement="bottom-start"  width="300">
+              <Input type="text" v-model="dataForm.parentName" :readonly='true'  :maxlength="8"></Input>
+              <div slot="content">
+                <Tree
+                :data='selectData'
+                :multiple='false'
+                @on-select-change='handleTreeSelectChange'
+                ></Tree>
+              </div>
+            </Poptip>
           </Form-item>
           <Form-item label="路由地址" prop="url">
-            <Input type="text"  :maxlength="8"></Input>
+            <Input v-model="dataForm.url" type="text"  :maxlength="8"></Input>
           </Form-item>
           <Form-item label="排序" prop="order">
-            <Input type="text"  :maxlength="8"></Input>
+            <Input v-model="dataForm.order" type="text"  :maxlength="8"></Input>
           </Form-item>
           <Form-item label="授权标识" prop="permission">
-             <Input type="text"  :maxlength="8"></Input>
+             <Input v-model="dataForm.permission" type="text"  :maxlength="8"></Input>
           </Form-item>
           <Form-item label="图标" prop="icon">
-             <Input type="text"  :maxlength="8"></Input>
+             <Input v-model="dataForm.icon" type="text"  :maxlength="8"></Input>
           </Form-item>
         </Form>
       </Modal>
@@ -48,16 +49,18 @@
 
 <script>
 import TreeSelect from '_c/tree-select'
+const PARENT_NAME_DEFAULT = '一级菜单'
 export default {
   name: '',
   data() {
     return {
-      treeSelected: [],
+      popVisible: false,
+      treeSelected: '',
       visible: false,
       loading: true,
       dataForm: {
         id: undefined, // 菜单/按钮id
-        parent_id: undefined, // 上级菜单id
+        parent_id: '0', // 上级菜单id
         title: '', // 菜单/按钮名称
         url: '', // 链接url
         permission: '', // 权限标识
@@ -74,7 +77,8 @@ export default {
         modify_time: null, // 修改时间
         create_time: null, // 创建时间
         version: '', // 乐观锁版本号
-        is_deleted: ''// 删除标记（0未删除，1已删除）
+        is_deleted: '', // 删除标记（0未删除，1已删除）
+        parentName: ''
       },
       tableData: [
         {
@@ -124,7 +128,38 @@ export default {
             ]
           }]
         }
-      ]
+      ],
+      selectData: [
+        {
+          id: 3,
+          title: '系统设置',
+          icon: '',
+          type: '菜单',
+          order: '0',
+          url: '',
+          permission: '',
+          children: [{
+            id: 31,
+            title: '用户管理',
+            icon: '',
+            type: '菜单',
+            order: '0',
+            url: '',
+            permission: ''
+          }]
+        }
+      ],
+      rules: {
+        type: [
+          { required: true, message: '必填项，不能为空', trigger: 'change' }
+        ],
+        title: [
+          { required: true, message: '必填项，不能为空', trigger: 'blur' }
+        ],
+        parentName: [
+          { required: true, message: '必填项，不能为空', trigger: 'change' }
+        ]
+      }
     }
   },
 
@@ -137,19 +172,17 @@ export default {
   methods: {
     init() {
       this.visible = true
+      this.parentName = PARENT_NAME_DEFAULT
     },
-    handleTreeSelectChange(list) {
-      // console.log('=-========', list)
+    menuListTreeSetDefaultHandle() {
+      this.dataForm.parent_id = '0'
+      this.dataForm.parentName = PARENT_NAME_DEFAULT
     },
-    handleTreeSelectExpand(item) {
-      // console.log('toggle expand', item);
-    },
-    handleTreeSelectCheckChange(selectedArray, item) {
-      // console.log(selectedArray, item)
-    },
-    handleTreeSelectClick(selectArray, item) {
-      this.treeSelected.push(item)
-      console.log(this.treeSelected)
+    // 绑定父级菜单选中值
+    handleTreeSelectChange(selectArray, item) {
+      this.dataForm.parent_id = item.id
+      this.dataForm.parentName = item.title
+      this.popVisible = false
     }
   }
 }
