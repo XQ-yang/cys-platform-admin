09e3be42f9f570a9e4ae8326ed35eb9e2719de9f
HuWeifei <1757581872@qq.com>
2019-06-19 17:43:46 +0800

    优化操作日志列表布局

---
+++ src/view/system/operlog/table-expand.vue
@@ -0,0 +1,26 @@
+<style scoped>
+    .expand-row{
+        margin-bottom: 16px;
+    }
+</style>
+<template>
+    <div>
+        <Row class="expand-row">
+            <Col>
+                <span class="expand-key">请求参数：{{ row.requestParam }}</span>
+            </Col>
+        </Row>
+        <Row>
+            <Col>
+                <span class="expand-key">错误消息：{{ row.responseData }}</span>
+            </Col>
+        </Row>
+    </div>
+</template>
+<script>
+export default {
+  props: {
+    row: Object
+  }
+}
+</script>
