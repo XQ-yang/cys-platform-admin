956aeb4730ca198846ce7a9f79740a304a24aeab
chendong <chendong19901127@126.com>
2019-07-03 15:26:39 +0800

    上传添加excel的支持

--- src/view/case/upload-file/index.vue
+++ src/view/case/upload-file/index.vue
@@ -6,8 +6,8 @@
         multiple
         type="drag"
         :max-size="10240"
-        accept=".pdf, .png, .jpg, .jpeg"
-        :format="['pdf','jpeg','png','jpg']"
+        accept=".pdf, .png, .jpg, .jpeg, .xls, .xlsx"
+        :format="['pdf','jpeg','png','jpg','xls','xlsx']"
         :before-upload="beforeUploadHandle"
         :on-exceeded-size="exceededSizeHandel"
         :headers="headers"
@@ -31,7 +31,7 @@ export default {
   data() {
     return {
       uploadList: [],
-      actionUrl: this.$baseUrl + '/upload/local_single',
+      actionUrl: this.$baseUrl + '/user/import_excel',
       headers: { Authorization: 'Bearer ' + getToken() }
     }
   },
@@ -64,7 +64,7 @@ export default {
     handleFormatError(file, fileList) {
       this.$Notice.error({
         title: '文件格式错误',
-        desc: '文件名为：' + file.name + '的格式错误，请选择.pdf, .png, .jpg, .jpeg格式文件上传！'
+        desc: '文件名为：' + file.name + '的格式错误，请选择.pdf, .png, .jpg, .jpeg,xls,xlsx式文件上传！'
       })
     }
   }
