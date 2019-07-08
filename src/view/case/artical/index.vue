c1d0a0fd291ea35b1e9c2feabdb8c7c335256e1a
thomas <cy_laq_2011@126.com>
2019-07-08 18:17:04 +0800

    1.暂存引入tinymce 测试相关API

---
+++ src/view/case/artical/index.vue
@@ -0,0 +1,25 @@
+<!-- create by lester -->
+<template>
+  <div></div>
+</template>
+
+<script>
+// import tinymce from 'tinymce'
+export default {
+  name: '',
+  data() {
+    return {
+    }
+  },
+
+  components: {},
+
+  mounted() {},
+  computed: {},
+
+  methods: {}
+}
+
+</script>
+<style>
+</style>
