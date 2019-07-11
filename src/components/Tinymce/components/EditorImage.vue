09f73fdd7ae7af7ce91628d3c5e0c6dee63e52bb
thomas <cy_laq_2011@126.com>
2019-07-11 18:03:11 +0800

    1.封装上传功能

--- src/components/Tinymce/components/EditorImage.vue
+++ src/components/Tinymce/components/EditorImage.vue
@@ -1,23 +1,48 @@
 <template>
   <div>
-    <Button type="primary" size="default" icon="md-cloud-upload">图片上传</Button>
+        <Upload
+        multiple
+        :max-size="10240"
+        accept=".pdf, .png, .jpg, .jpeg"
+        :format="['pdf','jpeg','png','jpg']"
+        :action="actionUrl"
+        :data="uploadData"
+        :on-success="handleSuccess"
+        >
+        <Button icon="ios-cloud-upload-outline">Upload files</Button>
+    </Upload>
   </div>
 </template>
 
 <script>
 export default {
   name: 'EditorSlideUpload',
-  data() {
-    return {
+  props: {
+    // OSS 返回的上传地址
+    actionUrl: {
+      type: String,
+      default: ''
+    },
+    // 上传时需要携带的数据
+    uploadData: {
+      type: Object,
+      default() {
+        return {}
+      }
     }
   },
+  data() {
+    return {
+      dialogVisible: false,
+      listObj: {},
+      fileList: []
+    }
+  },
+  methods: {
+    handleSuccess(res, file, fileList) {
 
-  components: {},
-
-  mounted() {},
-  computed: {},
-
-  methods: {}
+    }
+  }
 }
 
 </script>
