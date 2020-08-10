0ac4d80dabfd466176b9bab960c868a608bbc060
fuyingtao <meishadevs@gmail.com>
2020-08-10 14:07:57 +0800

    登录日志列表中固定表头，可以设置每页数据展示的条数

--- src/view/system/logininfo/index.vue
+++ src/view/system/logininfo/index.vue
@@ -24,7 +24,14 @@
         <Button @click="handleCancel" class="search-btn">重置</Button>
       </div>
       <!--列表 分页-->
-      <Table :data="list" :columns="tableColumns" :loading="tableLoading" border stripe>
+      <Table
+      border
+      stripe
+      :data="list"
+      :columns="tableColumns"
+      :loading="tableLoading"
+      :max-height="tableHeight"
+      ref="table">
         <template slot-scope="{ row, index }" slot="action">
         </template>
       </Table>
@@ -35,8 +42,10 @@
             :total="total"
             :current.sync="listQuery.pageNumber"
             :page-size.sync="listQuery.pageSize"
+            @on-page-size-change="pageSizeChange"
             @on-change="getList"
             show-total
+            show-sizer
             prev-text="上一页"
             next-text="下一页"
           ></Page>
@@ -55,6 +64,9 @@ export default {
   data() {
     return {
       list: [],
+
+      // 列表高度
+      tableHeight: 450,
 
       tableColumns: [
         {
@@ -162,6 +174,7 @@ export default {
   // 此钩子函数中一般会做一些ajax请求获取数据进行数据初始化
   // mounted在整个实例中只执行一次
   mounted() {
+    this.tableHeight = window.innerHeight - this.$refs.table.$el.offsetTop - 180
   },
 
   // 组件方法
@@ -198,6 +211,11 @@ export default {
       this.listQuery.status = ''
       this.listQuery.startTime = ''
       this.listQuery.endTime = ''
+      this.getList()
+    },
+
+    pageSizeChange(pagesize) {
+      this.listQuery.pageSize = pagesize
       this.getList()
     },
 
