face4d42a3503211b80aacd6436a9f0d10a70ada
fuyong <859050943@qq.com>
2021-03-10 10:19:03 +0800

    分离默认认证和web后台登录认证

--- src/api/user.js
+++ src/api/user.js
@@ -5,12 +5,14 @@ export const login = ({ username, password, generateKey, captchaCode }) => {
   let grant_type = 'password'
   let client_id = 'admin-web'
   let client_secret = '4402b06a67334d769fed712453284dae'
+  let auth_type = 'admin-web'
   const data = {
     username: username,
     password: password,
     generate_key: generateKey,
     captcha_code: captchaCode,
     grant_type: grant_type,
+    auth_type: auth_type,
     client_id: client_id,
     client_secret: client_secret
   }
